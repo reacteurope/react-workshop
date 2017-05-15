c379cbec0981c76bec03342eec09ff11c47c8003
Nik Graf <nik@nikgraf.com>
2017-05-15 21:21:59 +0200

    add solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import { compose, withState, withHandlers, renameProp } from 'recompose';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
-}
+const App = ({
+  thumbnailUrl,
+  name,
+  updateName,
+  favouriteText,
+  updateFavouriteText,
+  addListEntry,
+  favourites
+}) => (
+  <div>
+    <img src={thumbnailUrl} alt="Thumbnail Image" width="100" height="100" />
+    <input type="text" value={name} onChange={updateName} />
+    <form onSubmit={addListEntry}>
+      <input type="text" value={favouriteText} onChange={updateFavouriteText} />
+      <button type="submit">Add</button>
+    </form>
+    {favourites.map((favourite, index) => (
+      <div key={`${favourite}-${index}`}>{favourite}</div>
+    ))}
+  </div>
+);
 
-export default App;
+const enhance = compose(
+  renameProp('imageUrl', 'thumbnailUrl'),
+  withState('name', 'setName', props => props.name),
+  withState('favourites', 'setFavourites', []),
+  withState('favouriteText', 'setFavouriteText', ''),
+  withHandlers({
+    updateName: ({ setName }) => event => {
+      setName(event.target.value);
+    },
+    updateFavouriteText: ({ setFavouriteText }) => event => {
+      setFavouriteText(event.target.value);
+    },
+    addListEntry: ({
+      setFavourites,
+      favourites,
+      favouriteText,
+      setFavouriteText
+    }) => event => {
+      event.preventDefault();
+      setFavourites([favouriteText, ...favourites]);
+      setFavouriteText('');
+    }
+  })
+);
+
+export default enhance(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App name="Franz' Favourite Sports" imageUrl={'TODO add url'} />,
   document.getElementById('root')
 );
