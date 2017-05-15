b08c982b82de4fc809135035fb866a4efac2e3eb
Nik Graf <nik@nikgraf.com>
2017-05-15 19:10:09 +0200

    add solution

--- src/withDefaultCountProp.js
+++ src/withDefaultCountProp.js
@@ -1,1 +1,7 @@
 import React from 'react';
+
+const withDefaultCountProp = (Component, defaultNumber) => (props) => (
+  <Component count={defaultNumber} {...props} />
+)
+
+export default withDefaultCountProp;
