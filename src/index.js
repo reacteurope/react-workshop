2e2d717cdb928c88f082685389ed9b3bd3f5c5a8
Nik Graf <nik@nikgraf.com>
2017-05-17 11:31:40 +0200

    solution of 4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Recipes from './Recipes';
-// TODO import from apollo-client, react-apollo and inject the client via ApolloProvider
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={Recipes} />
-    </div>
-  </Router>,
+  () => <div>Hello</div>,
   document.getElementById('root')
 );
--- src/withLoading/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default Component => props => {
-  if (props.data.loading) {
-    return <div>Loading …</div>;
-  }
-  return <Component {...props} />;
-};
